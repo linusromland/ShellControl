3ee8b21284bbf04ce6a611dab2ac27420eea4243
Linus Romland <hello@linusromland.com>
2023-12-28 13:56:23 +0100

    Improved sidebar design

--- apps/client/src/components/Sidebar/Sidebar.tsx
+++ apps/client/src/components/Sidebar/Sidebar.tsx
@@ -1,29 +1,41 @@
 import { useState, useRef, useEffect } from 'react';
-import { Listbox, ListboxItem, ListboxSection } from '@nextui-org/react';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import { Avatar, Divider, Tooltip } from '@nextui-org/react';
+import { startCase } from 'lodash';
+import AddIcon from '@mui/icons-material/Add';
 import CreateProjectModal from '../CreateProjectModal/CreateProjectModal';
-import { useProjects } from '../../context/Projects.context';
+import { useProjects } from '../../contexts/Projects.context';
 import style from './Sidebar.module.css';
 
-dayjs.extend(relativeTime);
-
 export default function Sidebar(): JSX.Element {
-	const [collapsed] = useState(false);
+	// TODO: FIX THESE WITH ACTUAL VALUES
+	const active = 2;
+	const version = '1.0.0';
+
+	const [collapsed, setCollapsed] = useState(false);
 	const [width, setWidth] = useState(250);
 	const [resizing, setResizing] = useState(false);
 	const [showCreateProject, setShowCreateProject] = useState(false);
 
-	const { projects, fetchProjects } = useProjects();
+	const { projects, projectStatuses, fetchProjects } = useProjects();
 
 	const sidebarRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		const handleMouseMove = (e: MouseEvent) => {
 			if (resizing) {
+				const resizeWidth = e.clientX;
+
+				if (resizeWidth < 250) {
+					setCollapsed(true);
+				}
+
+				if (collapsed && resizeWidth > 250) {
+					setCollapsed(false);
+				}
+
 				const max = 250;
 				const min = 600;
-				setWidth(Math.max(Math.min(e.clientX, min), max));
+				setWidth(Math.max(Math.min(resizeWidth, min), max));
 			}
 		};
 
@@ -40,7 +52,7 @@ export default function Sidebar(): JSX.Element {
 			document.removeEventListener('mousemove', handleMouseMove);
 			document.removeEventListener('mouseup', handleMouseUp);
 		};
-	}, [resizing]);
+	}, [collapsed, resizing]);
 
 	useEffect(() => {
 		fetchProjects();
@@ -54,40 +66,78 @@ export default function Sidebar(): JSX.Element {
 			style={{ width }}
 		>
 			<div className={style.content}>
-				<h1 className={style.title}>ShellControl</h1>
-				{projects && (
-					<Listbox>
-						<ListboxSection
-							title='Projects'
-							showDivider
+				<div className={style.title}>
+					<Avatar src='favicon.png' />
+					{!collapsed && (
+						<div className={style.titleText}>
+							<h1>ShellControl</h1>
+							<p>Version {version}</p>
+						</div>
+					)}
+				</div>
+
+				<Divider />
+
+				<div className={style.projects}>
+					{projects.map((project) => (
+						<Tooltip
+							content={project.name}
+							key={project.id}
+							isDisabled={!collapsed}
+							placement='right'
+							showArrow
 						>
-							{projects.map((project) => (
-								<ListboxItem
-									key={project.id}
-									className={style.listboxItem}
-									textValue={project.name}
-								>
-									<span>{project.name}</span>
-									<span>{dayjs(project.createdAt).fromNow()}</span>
-								</ListboxItem>
-							))}
-						</ListboxSection>
-						<ListboxItem
-							key='create-project'
-							textValue='New Project'
+							<div
+								className={`${style.project} ${project.id === active ? style.activeProject : ''}`}
+								onClick={() => console.log(project)}
+							>
+								<Avatar
+									name={project.name}
+									color={project.id === active ? (collapsed ? 'primary' : 'secondary') : 'default'}
+								/>
+								{!collapsed && (
+									<div className={style.projectText}>
+										<span>{project.name}</span>
+										<span className={style.status}>
+											Status:{' '}
+											{startCase((projectStatuses[project.id] || 'STOPPED').toLowerCase())}
+										</span>
+									</div>
+								)}
+							</div>
+						</Tooltip>
+					))}
+				</div>
+
+				<Divider />
+
+				<div className={style.actions}>
+					<Tooltip
+						content='Create Project'
+						isDisabled={!collapsed}
+						placement='right'
+						showArrow
+					>
+						<div
+							className={style.action}
 							onClick={() => setShowCreateProject(true)}
 						>
-							<span>New Project</span>
-						</ListboxItem>
-					</Listbox>
-				)}
+							<Avatar
+								size='sm'
+								icon={<AddIcon fontSize='small' />}
+							/>
+							{!collapsed && <span>Create Project</span>}
+						</div>
+					</Tooltip>
+				</div>
 			</div>
+
 			<div
 				className={style.resizeHandle}
-				onMouseDown={() => {
-					setResizing(true);
-				}}
-			/>
+				onMouseDown={() => setResizing(true)}
+			>
+				<Divider orientation='vertical' />
+			</div>
 
 			<CreateProjectModal
 				isOpen={showCreateProject}
