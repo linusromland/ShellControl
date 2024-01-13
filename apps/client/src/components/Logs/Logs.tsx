97eba69ef62f5737f811ca8e4bdb422949106385
Linus Romland <hello@linusromland.com>
2024-01-13 14:53:02 +0100

    Improved logs display

--- apps/client/src/components/Logs/Logs.tsx
+++ apps/client/src/components/Logs/Logs.tsx
@@ -1,25 +1,33 @@
 import { Log } from '@local/shared/entities';
 import style from './Logs.module.css';
 import Scrollable from '../Scrollable/Scrollable';
+import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@nextui-org/react';
+import { useTheme } from '../../contexts/Theme.context';
 
 type LogsProps = {
 	logs: Log[];
 };
 
 const Logs = ({ logs }: LogsProps) => {
+	const { theme } = useTheme();
+
 	return (
 		<Scrollable
 			height='9rem'
 			autoScroll
 		>
-			{logs.map((log) => (
-				<p
-					key={log.id}
-					className={style.text}
-				>
-					{log.message}
-				</p>
-			))}
+			<Table aria-label='Logs'>
+				<TableHeader>
+					<TableColumn>Message</TableColumn>
+				</TableHeader>
+				<TableBody emptyContent='No logs'>
+					{logs.map((log) => (
+						<TableRow key={log.id}>
+							<TableCell className={style[`tableCell-${theme}`]}>{log.message}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 		</Scrollable>
 	);
 };
