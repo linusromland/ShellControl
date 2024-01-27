3d9e0e6223260d1a38e010d741685ae6688c8ac3
Linus Romland <hello@linusromland.com>
2024-01-27 10:14:19 +0100

    Added lockfile to apiManager

--- apps/api/apiManager/main.py
+++ apps/api/apiManager/main.py
@@ -1,33 +1,61 @@
 import subprocess
+import os
+import sys
 from pystray import Icon as icon, Menu as menu, MenuItem as item
 from PIL import Image
-
-image = Image.open("icon.png")
-api_service_executable_path = "../release/main-win.exe"
-
-print("Starting ShellControl Service...")
-
-# Start the API service
-# api_pid = subprocess.Popen([api_service_executable_path])
-print("ShellControl Service started successfully.")
+import portalocker
 
 
-def handle_exit(icon):
-    print("Exiting ShellControl Service...")
-    # api_pid.kill()
-    icon.stop()
+def start_service():
+    image = Image.open("icon.png")
+    api_service_executable_path = "../release/main-win.exe"
+
+    print("Starting ShellControl Service...")
+    api_pid = subprocess.Popen([api_service_executable_path])
+    print("ShellControl Service started successfully.")
+
+    def handle_open(icon):
+        print("Opening ShellControl...")
+
+    state = False
+
+    def handle_autoboot(icon, item):
+        nonlocal state
+        state = not item.checked
+
+    def handle_exit(icon):
+        print("Exiting ShellControl Service...")
+        api_pid.kill()
+        icon.stop()
+
+    icon('ShellControl Service', image, menu=menu(
+        item('Open ShellControl', handle_open),
+        item('Autostart on boot', handle_autoboot, checked=lambda item: state),
+        item('Exit', handle_exit))).run()
 
 
-state = False
+def single_instance_check():
+    lock_file_path = os.path.join(
+        os.path.expanduser("~"), ".ShellControlService.lock")
+    lock_file = open(lock_file_path, "w")
+
+    try:
+        portalocker.lock(lock_file, portalocker.LOCK_EX | portalocker.LOCK_NB)
+    except portalocker.LockException:
+        print("Another instance is already running. Exiting.")
+        sys.exit(1)
+
+    return lock_file
 
 
-def handle_autoboot(icon, item):
-    global state
-    state = not item.checked
+def main():
+    lock_file = single_instance_check()
+
+    start_service()
+
+    lock_file.close()
+    os.remove(lock_file.name)
 
 
-# Update the state in `on_clicked` and return the new state in
-# a `checked` callable
-icon('ShellControl Service', image, menu=menu(
-    item('Autostart on boot', handle_autoboot, checked=lambda item: state),
-    item('Exit', handle_exit))).run()
+if __name__ == "__main__":
+    main()
